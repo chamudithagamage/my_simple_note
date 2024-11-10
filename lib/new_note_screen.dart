a673e7eda6cfe89b64275208895fab067e75d431
Chamuditha Gamage <cnimsaragamage@gmail.com>
2024-11-11 00:33:11 +0530

    added multiple pages

--- lib/new_note_screen.dart
+++ lib/new_note_screen.dart
@@ -1,2 +1,34 @@
 import 'package:flutter/material.dart';
-import '';
+
+class NewNoteScreen extends StatefulWidget{
+
+  const NewNoteScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _NewNoteScreenState();
+}
+
+class _NewNoteScreenState extends State<NewNoteScreen>{
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Modify Entry'),
+      ),
+      
+      body: Padding(
+          padding: EdgeInsets.only(top:15.0, left:10.0, right: 10.0),
+        child:ListView(
+          //An array of widgets to mention priority by color
+          children:<Widget>[
+            //first element
+            ListTile(
+
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/note_list.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import '/new_note_screen.dart';
+
+class NoteList extends StatefulWidget{
+
+  const NoteList({super.key, required String title});
+
+  @override
+  State<StatefulWidget> createState()=> _NoteListState();
+}
+
+class _NoteListState extends State<NoteList>{
+
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context){
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Simple Note'),
+        backgroundColor: Colors.greenAccent,
+        elevation: 2.0,
+
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+            onPressed: ()=>{
+              debugPrint('Hamburger menu pressed'),
+            },
+        
+        ),
+
+      ),
+      body: getNoteListView(),
+
+        floatingActionButton: FloatingActionButton(onPressed: ()=>{
+          debugPrint('Floating Button Tapped'),
+        },
+          tooltip: 'Adds a new note',
+          child: Icon(Icons.edit),
+          shape: CircleBorder(),
+          backgroundColor: Colors.green,
+      ),
+    );
+  }
+  ListView getNoteListView(){
+    TextStyle? headingTitle = Theme.of(context).textTheme.titleLarge;
+    return ListView.builder(
+        itemCount: count,
+        itemBuilder: (BuildContext context, int position){
+          return Card(
+            color: Colors.white,
+            elevation: 1.0,
+              child: ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.cyan,
+                  child: Icon(Icons.edit_note_outlined),
+              ),
+                
+              title: Text('John Doe', style: headingTitle,),
+              subtitle: Text('Test Date'),
+              trailing: Icon(Icons.more_vert, color: Colors.grey),
+                //edit on tap
+                  onTap: ()=> debugPrint("List Tile Tapped"),
+
+            ),
+          );
+        },
+
+    );
+  }
+}
